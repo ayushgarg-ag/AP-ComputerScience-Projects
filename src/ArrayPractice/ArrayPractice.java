23ca8dd115f627830ea9f9ee264ca7341622dbe2
agarg01 <agarg01@P2P4DF36C.admin.bvsd.k12.co.us>
2017-10-17 12:44:46 -0600

    ArrayPractice

--- src/ArrayPractice/ArrayPractice.java
+++ src/ArrayPractice/ArrayPractice.java
@@ -4,14 +4,24 @@ public class ArrayPractice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String [] school = new String [3];
-		school[0] = "Peak";
-		school[1] = "to";
-		school[2] = "Peak";
-		
-		for (int x = 0; x < 3; x++) {
-			System.out.print(school[x] + " ");
+		// String [] school2 = new String [3];
+		// school2[0] = "Peak";
+		// school2[1] = "to";
+		// school2[2] = "Peak";
+
+		String[] school = { "Peak", "to", "Peak"};
+
+//		for (int x = 0; x < school.length; x++) {
+//			System.out.print(school[x] + " ");
+//		}
+		System.out.println(concat(school));
+	}
+	public static String concat(String[] arr) {
+		String y = new String();
+		for (int x = 0; x < arr.length; x++) {
+			y += arr[x] + " ";
 		}
+		return y;
 	}
 
 }
