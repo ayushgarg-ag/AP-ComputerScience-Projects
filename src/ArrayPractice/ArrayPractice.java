8f4329c6c00dfc24cdeb4ce3754acbc92a8ad340
agarg01 <agarg01@P2P4DF36C.admin.bvsd.k12.co.us>
2017-10-17 12:13:00 -0600

    FINAL Guess Runner

---
+++ src/ArrayPractice/ArrayPractice.java
@@ -0,0 +1,17 @@
+package ArrayPractice;
+
+public class ArrayPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String [] school = new String [3];
+		school[0] = "Peak";
+		school[1] = "to";
+		school[2] = "Peak";
+		
+		for (int x = 0; x < 3; x++) {
+			System.out.print(school[x] + " ");
+		}
+	}
+
+}
--- src/guess/GuessRunner.java
+++ src/guess/GuessRunner.java
@@ -11,52 +11,61 @@ public class GuessRunner {
 		Scanner reader = new Scanner(System.in);
 		int guesses = 0;
 
-		int num;
-		do {
-			System.out.println("Enter an integer");
-			try {
-				num = reader.nextInt();
-				if (num > 100 || num < 0) {
-					System.out.println("That is out of range!");
-				}
-				guesses++;
-			} catch (Exception e) {
-				String wrong = reader.nextLine();
-				System.out.println("You were supposed to enter an integer! \nYou entered: " + wrong + "\n" + e);
-				num = -1;
-			}
-			guesses++;
-		} while ((num < 0) || (num > 100));
-		Value guess = new Value(num);
+		String response;
+		Scanner reader2 = new Scanner(System.in);
 
-		while (val.compareTo(guess) != 0) {
-			if (val.compareTo(guess) > 0) {
-				System.out.println("The guessed number is too small.");
-				guesses++;
-			} else if (val.compareTo(guess) < 0) {
-				System.out.println("The guessed number is too large.");
-				guesses++;
-			}
+		do {
+			int num;
 			do {
 				System.out.println("Enter an integer");
 				try {
 					num = reader.nextInt();
-
+					if (num > 100 || num < 0) {
+						System.out.println("That is out of range!");
+					}
 				} catch (Exception e) {
 					String wrong = reader.nextLine();
 					System.out.println("You were supposed to enter an integer! \nYou entered: " + wrong + "\n" + e);
 					num = -1;
 				}
+				guesses++;
 			} while ((num < 0) || (num > 100));
-			guess = new Value(num);
+			Value guess = new Value(num);
 
+			while (val.compareTo(guess) != 0) {
+				if (val.compareTo(guess) > 0) {
+					System.out.println("The guessed number is too small.");
+					guesses++;
+				} else if (val.compareTo(guess) < 0) {
+					System.out.println("The guessed number is too large.");
+					guesses++;
+				}
+				do {
+					System.out.println("Enter an integer");
+					try {
+						num = reader.nextInt();
+
+					} catch (Exception e) {
+						String wrong = reader.nextLine();
+						System.out.println("You were supposed to enter an integer! \nYou entered: " + wrong + "\n" + e);
+						num = -1;
+					}
+				} while ((num < 0) || (num > 100));
+				guess = new Value(num);
+
+			}
+
+			if (val.equals(guess)) {
+				System.out.println("Correct!");
+				guesses++;
+			}
+
+			System.out.println("It took you " + guesses + " turns.");
+
+			System.out.println("Do you want to play again?");
+			response = reader2.next();
 		}
 
-		if (val.equals(guess)) {
-			System.out.println("Correct!");
-			guesses++;
-		}
-
-		System.out.println("It took you " + guesses + " turns.");
+		while (response.equals("yes"));
 	}
 }
