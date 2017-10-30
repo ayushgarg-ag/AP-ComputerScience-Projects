33191f62a2fda71cabe18d736e7bb7addb0a2b64
agarg01 <agarg01@P2P4DF36C.admin.bvsd.k12.co.us>
2017-10-30 12:30:22 -0600

    10/30

--- src/gradesArray/Runner.java
+++ src/gradesArray/Runner.java
@@ -7,25 +7,58 @@ public class Runner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		double grades [] = new double[3];
 		Scanner reader = new Scanner(System.in);
-
-		for (int i = 0; i < grades.length; i++) {
-			System.out.println("Enter a test score  (in decimals from 0.0 to 100.0)");
-			double num = reader.nextDouble();
-			if (num > 100.0 || num < 0.0) {
-				num = reader.nextDouble();
-			}
-			grades[i] = num;
+//		int count;
+		double grades[] = new double[10];
+//		do {
+//			System.out.println("Enter how many test scores (<=10)");
+//
+//			try {
+//				count = reader.nextInt();
+//				if (count > 10 || count <= 0) {
+//					System.out.println("That does not work!");
+//				}
+//			}
+//			catch (Exception e) {
+//				String wrong = reader.nextLine();
+//				System.out.println("You were supposed to enter a number! \nYou entered: " + wrong + "\n" + e);
+//				count = 11;
+//			}
+//		} while (count > 10 || count <= 0);
+		
+		double num;
+		for (int i = 0; i < grades.length; i++) {			
+			do {
+				System.out.println("Enter test scores");
+				try {
+					num = reader.nextDouble();
+					if (num < 0.0) {
+						
+					}
+					else if (num > 100.0) {
+						System.out.println("That is out of range!");
+					}
+				}
+				catch (Exception e) {
+					String wrong = reader.nextLine();
+					System.out.println("You were supposed to enter a number! \nYou entered: " + wrong + "\n" + e);
+					num = -1.0;
+				}
+			} while ((num < 0.0) || (num > 100.0));
+			
+//			if (num > 0.0) {
+				grades[i] = num;				
+//			}
 		}
 		System.out.println(Arrays.toString(grades));
 		double [] copy = copier (grades);
+		int length = copy.length;
+		System.out.println("The maximum score is: " + max(copy, length));
+		System.out.println("The minimum score is: " + min(copy, length));
+		System.out.println("The mean is: " + mean(copy, length));
+		System.out.println("The median is: " + median(copy, length));
 		
-		System.out.println("The maximum score is: " + max(copy));
-		System.out.println("The minimum score is: " + min(copy));
-		System.out.println("The mean is: " + mean(copy));
-		System.out.println("The median is: " + median(copy));
-		
+
 	}
 	
 	public static double[] copier (double[] arr) {
@@ -33,9 +66,9 @@ public class Runner {
 		return arr2;
 	}
 	
-	public static double min (double[] arr) {
+	public static double min (double[] arr, int size) {
 		double min = arr[0];
-		for (int i = 0; i < arr.length; i++) {
+		for (int i = 0; i < size; i++) {
 			if (arr[i] < min) {
 				min = arr[i];
 			}
@@ -43,9 +76,9 @@ public class Runner {
 		return min;
 	}
 
-	public static double max (double[] arr) {
+	public static double max (double[] arr, int size) {
 		double max = arr[0];
-		for (int i = 0; i < arr.length; i++) {
+		for (int i = 0; i < size; i++) {
 			if (arr[i] > max) {
 				max = arr[i];
 			}
@@ -53,18 +86,26 @@ public class Runner {
 		return max;
 	}
 	
-	public static double mean (double[] arr) {
+	public static double mean (double[] arr, int size) {
 		double sum = 0;
-		for (int i = 0; i < arr.length; i++) {
+		for (int i = 0; i < size; i++) {
 			sum += arr[i];
 		}
-		double mean = sum/(arr.length);
+		double mean = sum/(size);
 		return mean;
 	}
 	
-	public static double median (double[] arr) {
+	public static double median (double[] arr, int size) {
 		Arrays.sort (arr);
-		double median = arr[1];
+		double median;
+		if (size % 2 == 0) {
+			double first = arr[size/2];
+			double second = arr[(size/2) - 1];
+			median = (first + second) / 2.0;
+		}
+		else {
+			median = arr[size/2];
+		}
 		return median;
 	}
 	
