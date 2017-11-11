d3c1620b9a84f367a14091ac5c30eea77973ab06
garghome <garghome@GARG-HOME.domain>
2017-11-10 23:35:40 -0700

    DoubleList Project - Extensions: toArray & contain

--- src/doubleList/DoubleList.java
+++ src/doubleList/DoubleList.java
@@ -4,6 +4,7 @@ package doubleList;
  * DoubleList is a class that behaves much like a variable-sized array – a runner will be able to create a new empty DoubleList and then add items to the list, access the values in the list, change items in the list, get the effective size of the list, determine the maximum value, the minimum value, etc.
  * @author agarg01
  * 10/31
+ * Extensions: toArray method and contain method
  */
 public class DoubleList {
 	
@@ -11,7 +12,7 @@ public class DoubleList {
 	private int mySize;
 	
 	/**
-	 * create a MyList with an empty array of doubles of size 20, a mySize with a value of 0
+	 * Creates a MyList with an empty array of doubles of size 20, a mySize with a value of 0
 	 */
 	public DoubleList() {
 		myList = new double[20];
@@ -19,14 +20,20 @@ public class DoubleList {
 	}
 	
 	/**
-	 * create a MyList with an empty array of doubles of size i, a mySize with a value of 0
-	 * @param i
+	 * Initialize myList as a new double array of the specified size
+	 * @param i: the size of the Double List array
+	 * Precondition: the specified value is not 0
 	 */
 	public DoubleList (int i) {
 		myList = new double[i];
 		mySize = 0;
 	}
 	
+	/**
+	 * Initializes myList as a new double array the size of the passed array plus 20
+	 * @param arr: a double array that is added to the Double List
+	 * Postcondition: the values from the passed array are added to the DoubleList
+	 */
 	public DoubleList (double[] arr) {
 		myList = new double[arr.length + 20];
 		mySize = 0;
@@ -54,7 +61,7 @@ public class DoubleList {
 	
 	/**
 	 * Adds an item to the end of the DoubleList
-	 * @param item the value to be added
+	 * @param d item the value to be added
 	 * Precondition: list is not full
 	 * Postcondition: item is added to the end of the list
 	 * @throws RuntimeException if list is full
@@ -188,6 +195,13 @@ public class DoubleList {
 		return min;
 	}
 
+	/**
+	 * Return the value of the DoubleList stored at that index
+	 * @param index the index of the value that will be returned
+	 * Precondition: the list is not empty and the requested index is not out of bounds
+	 * @return the value at the provided index
+	 * @throws IndexOutOfBoundsException if list is full or if the index is out of bounds
+	 */
 	public double get(int index) {
 		if (mySize < 0 || index >= mySize ) {
 			throw new IndexOutOfBoundsException("The index is out of bounds.");
@@ -195,6 +209,14 @@ public class DoubleList {
 		return myList[index];
 	}
 	
+	/** 
+	 * Sets the value of the DoubleList stored at the index to the passed value
+	 * @param index: the index of the value that will be set
+	 * @param d: the value that will be set
+	 * Precondition: the requested index is in the effective size
+	 * Postcondition: the specified value is set at the specified index
+	 * @throws IndexOutOfBoundsException if the index is out of the effective size
+	 */
 	public void set(int index, double d) {
 		if (index >= mySize)  {
 			throw new IndexOutOfBoundsException("The index is not in the effective size.");			
@@ -202,48 +224,74 @@ public class DoubleList {
 			myList[index] = d;
 	}
 	
+	/**
+	 * Inserts the item into the array and moves all the following items one position to "make room" for the item
+	 * @param index: the index of the value that will be added
+	 * @param d: the value that will be added
+	 * Precondition: the specified index is not out of bounds and there is room in the array to add one more item
+	 * Postcondition: the specified value is inserted into the array at the specified index and all the following items move one position to "make room" for the item
+	 * @throws IndexOutOfBoundsException if there is no room in the array or if the index is out of bounds
+	 */
 	public void add (int index, double d) {
+		if (mySize + 1 > maxSize()) {
+			throw new IndexOutOfBoundsException("There is no room in the array.");
+		}
+		
 		if (index > mySize) {
 			throw new IndexOutOfBoundsException("The specified index is out of bounds.");
 		}
 		
-		if (mySize > maxSize()) {
-			throw new IndexOutOfBoundsException("There is no room in the array.");
-//			throw new RejectedExecutionException("There is no room in the array.");
-		}
-		
-		for (int i = index; i < mySize - index; i++) {
-			myList[i + 1] = myList[i];
+		mySize++;
+		for (int i = mySize-1; index < i; i--) {
+			myList[i] = myList[i-1];
 		}
 		myList[index] = d;
 	}
 	
+	/**
+	 * Removes the item located at the specified index and moves all the following items down one position
+	 * @param index: the index of the value that will be removed
+	 * Precondition: the specified index is not out of bounds
+	 * Postcondition: the value at the specified is removed from the array and all the following items move down one position
+	 * @throws IndexOutOfBoundsException if the index is out of bounds
+	 */
 	public void remove(int index) {
 		if (index >= mySize) {
 			throw new IndexOutOfBoundsException("The specfied index is out of bounds.");
 		}
-		for (int i = index; i < mySize - index; i--) {
+		for (int i = index; i < mySize; i++) {
 			myList[i] = myList[i+1];
 		}
 		mySize--;			
 	}
 	
+	/**
+	 * Removes all items in the list
+	 * Precondition: the list is not already empty
+	 * Postcondition: the list is cleared
+	 */
 	public void clear() {
-		for (int i = 0; i < mySize; i++) {
-			myList[i] = 0.0;
-		}
+		mySize = 0;
 	}
 	
+	/**
+	 * Returns true only if the passed DoubleList contains identical item to "this"  DoubleList
+	 * @param o: another DoubleList
+	 * @return a boolean depending on whether the passed DoubleList contains identical item to "this"  DoubleList
+	 */
 	public boolean equals(DoubleList o) {
-		boolean same = true;
 		for (int i = 0; i < mySize; i++) {
 			if (myList[i] != o.get(i)) {
 				return false;
 			}
 		}
-		return same;
+		return true;
 	}
 	
+	/**
+	 * Returns true only if the DoubleList contains no items
+	 * @return a boolean depending on whether the list is empty or not
+	 */
 	public boolean isEmpty() {
 		if (mySize == 0) {
 			return true;
@@ -253,6 +301,10 @@ public class DoubleList {
 		}
 	}
 	
+	/**
+	 * Returns an array containing only the current items in the DoubleList
+	 * @return an array containing only the current items in the DoubleList
+	 */
 	public double[] toArray() {
 		double[] arr = new double[mySize]; 
 		for (int i = 0; i < mySize; i++) {
@@ -261,6 +313,11 @@ public class DoubleList {
 		return arr;
 	}
 	
+	/**
+	 * Returns a boolean depending on whether or not that double is in the DoubleList
+	 * @param d: the double that is being checked to see if it is in the DoubleList
+	 * @return a boolean depending on whether or not that double is in the DoubleList
+	 */
 	public boolean contains(double d) {
 		for (int i = 0; i < mySize; i++) {
 			if (myList[i] == d) {
--- src/doubleList/ListTester.java
+++ src/doubleList/ListTester.java
@@ -1,130 +1,215 @@
 package doubleList;
 
+import java.util.Arrays;
+
 public class ListTester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println ("Testing Constructor");
-		DoubleList aList = new DoubleList ();
-		System.out.println ("size: " + aList.size());
-		System.out.println ("max size: " + aList.maxSize());
-		
-		System.out.println ("Testing add and toString");
-		System.out.println ("List is: " + aList);
-		aList.add (12.0);
-		aList.add (-1.0E305);
-		System.out.println ("List is: " + aList);
-		for (int i = 0; i<18; i++) aList.add ((double)i);
+		// Constructor
+		System.out.println("Testing Constructor");
+		DoubleList aList = new DoubleList();
+		System.out.println("size: " + aList.size());
+		System.out.println("max size: " + aList.maxSize());
+
+		// Add and toString
+		System.out.println("\nTesting add and toString");
+		System.out.println("List is: " + aList);
+		aList.add(12.0);
+		aList.add(-1.0E305);
+		System.out.println("List is: " + aList);
+		for (int i = 0; i < 18; i++)
+			aList.add((double) i);
 		try {
-		   aList.add (100.0);  // generates an Exception
+			aList.add(100.0); // generates an Exception
+		} catch (Exception e) {
+			System.out.println("Error: " + e);
 		}
-		catch (Exception e) {
-		   System.out.println ("Error: " + e);
-		}
-		System.out.println ("List is: " + aList);
-		
-		System.out.println ("Testing Constructor 2");
-		DoubleList aList2 = new DoubleList (5);
-		System.out.println ("size: " + aList2.size());
-		System.out.println ("max size: " + aList2.maxSize());
-		
+		System.out.println("List is: " + aList);
+
+		// Constructor 2
+		System.out.println("\nTesting Constructor 2");
+		DoubleList aList2 = new DoubleList(5);
+		System.out.println("size: " + aList2.size());
+		System.out.println("max size: " + aList2.maxSize());
+
+		// Constructor 3
 		double[] arr = new double[5];
 		for (int i = 0; i < arr.length; i++) {
-			arr[i] = (double)i;
+			arr[i] = (double) i;
 		}
-		System.out.println ("Testing Constructor 3");
-		DoubleList aList3 = new DoubleList (arr);
-		System.out.println ("size: " + aList3.size());
-		System.out.println ("max size: " + aList3.maxSize());
-		System.out.println ("List is: " + aList3+"\n");
-		
+		System.out.println("\nTesting Constructor 3");
+		DoubleList aList3 = new DoubleList(arr);
+		System.out.println("size: " + aList3.size());
+		System.out.println("max size: " + aList3.maxSize());
+		System.out.println("Constructs List 3 with an array arr");
+		System.out.println("List is: " + aList3 + "\n");
+
+		// Remove
 		double[] arr2 = new double[5];
 		for (int i = 0; i < arr2.length; i++) {
-			arr2[i] = (double)i;
+			arr2[i] = (double) i;
 		}
-		System.out.println ("Testing Remove Method");
-		DoubleList aList4 = new DoubleList (arr2);
+		System.out.println("Testing Remove Method");
+		DoubleList aList4 = new DoubleList(arr2);
 		double r;
-
 		for (int i = 0; i < arr2.length; i++) {
 			r = aList4.remove();
-			System.out.println("Removed Value: "+r);
+			System.out.println("Removed Value: " + r);
 		}
 		try {
-		   aList4.remove();  // generates an Exception
-		}
-		catch (Exception e) {
-		   System.out.println ("Error: " + e);
-		}
-		System.out.println ("size: " + aList4.size());
-		System.out.println ("max size: " + aList4.maxSize());
-		System.out.println ("List is: " + aList4 + "\n");
-
-		
-		System.out.println ("Testing indexOf Method");
-		System.out.println ("First occurence of 0.0 is " + aList3.indexOf(0.0));
-		System.out.println ("First occurence of 2.0 is " + aList3.indexOf(2.0));
-		System.out.println ("First occurence of 5.0 is " + aList3.indexOf(5.0));
-		
-		System.out.println ("\nList is: " + aList3);
-		System.out.println ("Testing sum Method");
-		System.out.println ("The sum of aList3 is " + aList3.sum());
-		System.out.println ("The sum of aList4 is " + aList4.sum());
-
-		
-		System.out.println ("\nTesting mean Method");
-		System.out.println ("The mean of aList3 is " + aList3.mean());
-		System.out.println ("The mean of aList4 is ");
-		try {
-			aList4.mean();  // generates an Exception
-		}
-		catch (Exception e) {
-		   System.out.println ("Error: " + e);
-		}
-		
-		System.out.println ("\nTesting maximum Method");
-		System.out.println ("The maximum of aList3 is " + aList3.maximum());
-		System.out.println ("The maximum of aList4 is ");
-		try {
-			aList4.maximum();  // generates an Exception
-		}
-		catch (Exception e) {
-		   System.out.println ("Error: " + e);
-		}
-		
-		System.out.println ("\nTesting minimum Method");
-		System.out.println ("The minimum of aList3 is " + aList3.minimum());
-		System.out.println ("The minimum of aList4 is ");
-		try {
-			aList4.minimum();  // generates an Exception
-		}
-		catch (Exception e) {
-		   System.out.println ("Error: " + e);
+			aList4.remove(); // generates an Exception
+		} catch (Exception e) {
+			System.out.println("Error: " + e);
 		}
 
-		System.out.println ("\nTesting get Method");
-		System.out.println ("The value at index 1 is " + aList3.get(1));
-		System.out.println ("The value at index 5 is ");
+		// IndexOf
+		System.out.println("\nTesting indexOf Method (-1 means not found)");
+		System.out.println("First occurence of 0.0 is " + aList3.indexOf(0.0));
+		System.out.println("First occurence of 2.0 is " + aList3.indexOf(2.0));
+		System.out.println("First occurence of 5.0 is " + aList3.indexOf(5.0));
+
+		// Sum
+		System.out.println("\nTesting sum Method");
+		System.out.println("List 3 is: " + aList3);
+		System.out.println("The sum of List 3 is " + aList3.sum());
+		System.out.println("List 4 is: " + aList4);
+		System.out.println("The sum of List 4 is " + aList4.sum());
+
+		// Mean
+		System.out.println("\nTesting mean Method");
+		System.out.println("The mean of List 3 is " + aList3.mean());
+		System.out.println("The mean of List 4 is ");
 		try {
-			aList3.get(5);  // generates an Exception
+			aList4.mean(); // generates an Exception
+		} catch (Exception e) {
+			System.out.println("Error: " + e);
 		}
-		catch (Exception e) {
-		   System.out.println ("Error: " + e);
+
+		// Maximum
+		System.out.println("\nTesting maximum Method");
+		System.out.println("The maximum of List 3 is " + aList3.maximum());
+		System.out.println("The maximum of List 4 is ");
+		try {
+			aList4.maximum(); // generates an Exception
+		} catch (Exception e) {
+			System.out.println("Error: " + e);
 		}
-		
-		System.out.println ("\nTesting set Method");
-		System.out.println ("Sets the value at index 0 to 1.0");
+
+		// Minimum
+		System.out.println("\nTesting minimum Method");
+		System.out.println("The minimum of List 3 is " + aList3.minimum());
+		System.out.println("The minimum of List 4 is ");
+		try {
+			aList4.minimum(); // generates an Exception
+		} catch (Exception e) {
+			System.out.println("Error: " + e);
+		}
+
+		// Get
+		System.out.println("\nTesting get Method");
+		System.out.println("The value at index 1 of List 3 is " + aList3.get(1));
+		System.out.println("The value at index 5 of List 3 is ");
+		try {
+			aList3.get(5); // generates an Exception
+		} catch (Exception e) {
+			System.out.println("Error: " + e);
+		}
+
+		// Set
+		System.out.println("\nTesting set Method");
+		System.out.println("Sets the value at index 0 to 1.0");
 		aList3.set(0, 1.0);
-		System.out.println ("List is: " + aList3+"\n");
-
-		System.out.println ("Sets the value at index 6 to 6.0");
+		System.out.println("List 3 is: " + aList3);
+		System.out.println("Tries to set the value at index 6 to 6.0");
 		try {
-			aList3.set(6, 6.0);  // generates an Exception
-		}
-		catch (Exception e) {
-		   System.out.println ("Error: " + e);
+			aList3.set(6, 6.0); // generates an Exception
+		} catch (Exception e) {
+			System.out.println("Error: " + e);
 		}
 
+		// Overloaded Add
+		double[] arr5 = new double[5];
+		for (int i = 0; i < arr5.length; i++) {
+			arr5[i] = (double) i;
+		}
+		DoubleList aList5 = new DoubleList(arr5);
+		System.out.println("\nTesting overloaded add method");
+		System.out.println("List 5 is: " + aList5);
+		System.out.println("Adding 12.0 to the index of 1: ");
+		aList5.add(1, 12.0);
+		System.out.println(aList5);
+		System.out.println("Trying to add a value to an index that is bigger than mySize: ");
+		try {
+			aList5.add(7, 23.0); // generates an Exception
+		} catch (Exception e) {
+			System.out.println("Error: " + e);
+		}
 
+		DoubleList aList6 = new DoubleList(5);
+		for (int i = 0; i < aList6.maxSize(); i++) {
+			aList6.add(i, (int) i);
+		}
+		System.out.println("List 6 is: " + aList6);
+		System.out.println("Trying to add a value to the index of 6 which is bigger than the maxSize: ");
+		try {
+			aList6.add(16, 23.0); // generates an Exception
+		} catch (Exception e) {
+			System.out.println("Error: " + e);
+		}
+
+		// Overloaded Remove
+		System.out.println("\nTesting overloaded remove method");
+		System.out.println("List 5 is: " + aList5);
+		System.out.println("Removing value at the index of 1: ");
+		aList5.remove(1);
+		System.out.println(aList5);
+		System.out.println("Trying to remove a value from an index that is bigger than mySize: ");
+		try {
+			aList5.remove(7); // generates an Exception
+		} catch (Exception e) {
+			System.out.println("Error: " + e);
+		}
+
+		// Clear
+		System.out.println("\nTesting clear method");
+		System.out.println("List 6 is: " + aList6);
+		System.out.println("Clears list 6");
+		aList6.clear();
+		System.out.println("List 6 is: " + aList6);
+
+		// IsEmpty
+		System.out.println("\nTesting isEmpty method");
+		System.out.println("List 5 is empty: " + aList5.isEmpty());
+		System.out.println("List 6 is empty: " + aList6.isEmpty());
+
+		// Equals
+		System.out.println("\nTesting equals method");
+		double[] arr7 = new double[5];
+		for (int i = 0; i < arr7.length; i++) {
+			arr7[i] = (double) i;
+		}
+		DoubleList aList7 = new DoubleList(arr7);
+		DoubleList aList8 = new DoubleList(arr7);
+		System.out.println("List 7 is: " + aList7);
+		System.out.println("List 8 is: " + aList8);
+		System.out.println("aList7 equals aList8: " + aList7.equals(aList8));
+		System.out.println("Adds a value to aList7");
+		aList7.add(2, 10.0);
+		System.out.println("List 7 is: " + aList7);
+		System.out.println("List 8 is: " + aList8);
+		System.out.println("aList7 equals aList8: " + aList7.equals(aList8));
+
+		// ToArray
+		System.out.println("\nTesting toArray method");
+		System.out.println("List 5 is: " + aList5);
+		double[] a = aList5.toArray();
+		System.out.println("aList5 as an array: \n" + Arrays.toString(a));
+
+		// Contains
+		System.out.println("\nTesting contains method");
+		System.out.println("List 5 is: " + aList5);
+		System.out.println("2.0 is in list 5: " + aList5.contains(2.0));
+		System.out.println("5.0 is in list 5: " + aList5.contains(5.0));
 	}
 }
