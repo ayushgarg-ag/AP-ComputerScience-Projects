b1255c70eee5c4b3104b2cd1508a1bed73285656
agarg01 <agarg01@P2P4DF36C.admin.bvsd.k12.co.us>
2017-11-13 11:46:54 -0700

    Final PennyPitchRunner

--- src/pennyPitch/PennyPitchRunner.java
+++ src/pennyPitch/PennyPitchRunner.java
@@ -12,18 +12,18 @@ public class PennyPitchRunner {
 			for (int c = 0; c < board[0].length; c++) {
 				if (r == 0 || r == 4 || c == 0 || c == 4) {
 					board[r][c] = new Square(1);
-					System.out.print(board[r][c].toString());
+					System.out.print(board[r][c].toString()+" ");
 				} else if (r == 1 || r == 3 || c == 1 || c == 3) {
 					board[r][c] = new Square(2);
-					System.out.print(board[r][c].toString());
+					System.out.print(board[r][c].toString()+" ");
 				} else {
 					board[r][c] = new Square(3);
-					System.out.print(board[r][c].toString());
+					System.out.print(board[r][c].toString()+" ");
 				}
 			}
 			System.out.println();
 		}
-		// System.out.println(board.getScore());
+
 		for (int i = 0; i < 5; i++) {
 
 			Scanner reader = new Scanner(System.in);
@@ -40,7 +40,7 @@ public class PennyPitchRunner {
 			board[row][col].setOccupied(true);
 			for (int r = 0; r < board.length; r++) {
 				for (int c = 0; c < board[0].length; c++) {
-					System.out.print(board[r][c]);
+					System.out.print(board[r][c]+" ");
 				}
 				System.out.println();
 			}
