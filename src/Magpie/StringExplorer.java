f403675278db621d0a0ad05c40f77a80a1f58bed
agarg01 <agarg01@P2P4DF36C.admin.bvsd.k12.co.us>
2017-11-28 12:32:25 -0700

    Magpie 11/28

---
+++ src/Magpie/StringExplorer.java
@@ -0,0 +1,34 @@
+package Magpie;
+/**
+ * A program to allow students to try out different 
+ * String methods. 
+ * @author Laurie White
+ * @version April 2012
+ */
+public class StringExplorer
+{
+
+	public static void main(String[] args)
+	{
+		String sample = "The quick brown fox jumped over the lazy dog.";
+		
+		//  Demonstrate the indexOf method.
+		int position = sample.indexOf("quick");
+		System.out.println ("sample.indexOf(\"quick\") = " + position);
+		
+		//  Demonstrate the toLowerCase method.
+		String lowerCase = sample.toLowerCase();
+		System.out.println ("sample.toLowerCase() = " + lowerCase);
+		System.out.println ("After toLowerCase(), sample = " + sample);
+		
+		//  Try other methods here:
+		int notFoundPsn = sample.indexOf("slow");
+		System.out.println("sample.indexOf(\"slow\") = " + notFoundPsn);
+		
+		// indexOf with two parameters - Returns the index within this string of the first occurrence of the specified substring, starting at the specified index.
+		int findO = sample.indexOf("o", 13);
+		System.out.println("sample.indexOf(\"slow\") = " + findO);
+		
+	}
+
+}
