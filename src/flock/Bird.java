90a2b6b23be009179c426466dd4d54310d249005
agarg01 <agarg01@P2P4DF36C.admin.bvsd.k12.co.us>
2018-01-24 12:45:10 -0700

    Bird Flock 1/24

--- src/flock/Bird.java
+++ src/flock/Bird.java
@@ -6,28 +6,49 @@ import java.util.ArrayList;
 import info.gridworld.actor.Actor;
 import info.gridworld.grid.Location;
 
+public class Bird extends Actor {
+	public static boolean flock = true;
 
-public class Bird extends Actor{
-	public static boolean flock = false;
-	
-	
+	public Bird() {
+		this.setColor(Color.GREEN);
+	}
+
 	public static boolean getFlock() {
 		return Bird.flock;
 	}
+
 	public static void setFlock(boolean flock) {
 		Bird.flock = flock;
 	}
-	
+
 	public void act() {
 		if (Bird.flock == true) {
-			this.setColor (Color.BLUE);
+			this.setColor(Color.GREEN);
 			ArrayList<Location> empty = this.getGrid().getEmptyAdjacentLocations(this.getLocation());
-			
-			int rand = (int)Math.random()*empty.size();
-		}
-		else {
-			this.setColor (Color.RED);
-			
+			if (empty.size() != 0) {
+				int rand = (int) (Math.random() * empty.size());
+				Location loc = empty.get(rand);
+				if (loc.getRow() + 1 == this.getLocation().getRow()) {
+					setDirection(this.getLocation().getDirectionToward(loc));
+					moveTo(loc);
+				}
+			}
+
+		} else {
+			this.setColor(Color.RED);
+			Location south = this.getLocation().getAdjacentLocation(Location.SOUTH);
+			if (this.getGrid().isValid(south)) {
+				if (this.getGrid().get(south) == null) {
+					moveTo(south);
+				} else {
+					Location east = this.getLocation().getAdjacentLocation(Location.EAST);
+					if (this.getGrid().isValid(east)) {
+						if (this.getGrid().get(east) == null) {
+							moveTo(east);
+						}
+					}
+				}
+			}
 		}
 	}
 }
