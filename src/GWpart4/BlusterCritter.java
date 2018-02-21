9712926dea364f94f2ddd2bbc7334e84d7941b99
ayushgarg <ayushgarg@ayushs-mbp.domain>
2018-02-20 23:49:15 -0700

    Home 2/20

--- src/GWpart4/BlusterCritter.java
+++ src/GWpart4/BlusterCritter.java
@@ -1,5 +1,6 @@
 package GWpart4;
 
+import java.awt.Color;
 import java.util.ArrayList;
 
 import info.gridworld.actor.*;
@@ -8,75 +9,83 @@ import info.gridworld.world.*;
 import info.gridworld.gui.*;
 
 public class BlusterCritter extends Critter {
-	private static final int c = 3;
+	protected static int c;
+
+	public BlusterCritter() {
+		super();
+		c = 3;
+	}
+	public BlusterCritter (int courage) {
+		super();
+		c = courage;
+	}
+
 	
-    public void act()
-    {
-        if (getGrid() == null)
-            return;
-        ArrayList<Actor> actors = getActors();
-        processActors(actors);
-        ArrayList<Location> moveLocs = getMoveLocations();
-        Location loc = selectMoveLocation(moveLocs);
-        makeMove(loc);
-    }
-    public ArrayList<Actor> getActors()
-    {
-    	Location loc = this.getLocation();
-    	int col = loc.getCol();
-    	int row = loc.getRow();
-    	
-    	ArrayList<Actor> topright = getGrid().getNeighbors(new Location(row-1, col+1));
-       	ArrayList<Actor> topleft = getGrid().getNeighbors(new Location(row-1, col-1));
-       	ArrayList<Actor> bottomright = getGrid().getNeighbors(new Location(row+1, col+1));
-       	ArrayList<Actor> bottomleft = getGrid().getNeighbors(new Location(row+1, col-1));
-           	
-//    	getGrid().getNeighbors(loc);
-//    	for (int r = -2; r < 2; r++) {
-//    		Location l = 
-//    	}
-//    	
-//    	
-//    	ArrayList<Actor> all;
-//    	ArrayList<Actor> each;
-//    	ArrayList<Actor> crit= getGrid().getNeighbors(getLocation());
-//    	for (int i = 0; i < crit.size(); i++) {
-//    		Location loc = crit.get(i).getLocation();
-//			each.add((getGrid().getNeighbors(loc)));
-//			for (int n = 0; n < each.size(); n++) {
-//				
-//			}
-//		}
-    }
+	public ArrayList<Actor> getActors() {
+		Location loc = this.getLocation();
+		int col = loc.getCol();
+		int row = loc.getRow();
 
-    public void processActors(ArrayList<Actor> actors)
-    {
-        for (Actor a : actors)
-        {
-            if (!(a instanceof Rock) && !(a instanceof Critter))
-                a.removeSelfFromGrid();
-        }
-    }
+		ArrayList<Actor> all = new ArrayList<Actor>();
+		ArrayList<Actor> topright = getGrid().getNeighbors(new Location(row - 1, col + 1));
+		ArrayList<Actor> topleft = getGrid().getNeighbors(new Location(row - 1, col - 1));
+		ArrayList<Actor> bottomright = getGrid().getNeighbors(new Location(row + 1, col + 1));
+		ArrayList<Actor> bottomleft = getGrid().getNeighbors(new Location(row + 1, col - 1));
 
-    public ArrayList<Location> getMoveLocations()
-    {
-        return getGrid().getEmptyAdjacentLocations(getLocation());
-    }
+		for (int i = 0; i < topright.size(); i++) {
+			all.add(topright.get(i));
+		}
+		for (int i = 0; i < topleft.size(); i++) {
+			all.add(topleft.get(i));
+		}
+		for (int i = 0; i < bottomright.size(); i++) {
+			all.add(bottomright.get(i));
+		}
+		for (int i = 0; i < bottomleft.size(); i++) {
+			all.add(bottomleft.get(i));
+		}
+		for (Actor a: all) {
+			System.out.println(a);
+			
+		}
+		return all;
+		// getGrid().getNeighbors(loc);
+		// for (int r = -2; r < 2; r++) {
+		// Location l =
+		// }
+		//
+		//
+		// ArrayList<Actor> all;
+		// ArrayList<Actor> each;
+		// ArrayList<Actor> crit= getGrid().getNeighbors(getLocation());
+		// for (int i = 0; i < crit.size(); i++) {
+		// Location loc = crit.get(i).getLocation();
+		// each.add((getGrid().getNeighbors(loc)));
+		// for (int n = 0; n < each.size(); n++) {
+		//
+		// }
+		// }
 
-    public Location selectMoveLocation(ArrayList<Location> locs)
-    {
-        int n = locs.size();
-        if (n == 0)
-            return getLocation();
-        int r = (int) (Math.random() * n);
-        return locs.get(r);
-    }
+	}
 
-    public void makeMove(Location loc)
-    {
-        if (loc == null)
-            removeSelfFromGrid();
-        else
-            moveTo(loc);
-    }
+	public void processActors(ArrayList<Actor> actors) {
+		int count = 0;
+		for (Actor a : actors) {
+			if (a instanceof Critter) {
+				count++;
+			}
+		}
+		System.out.println("C value: " + c);
+		System.out.println("Number of critters: " + c);
+
+		if (c > count) {
+			Color col = getColor().brighter();
+			System.out.println(col);
+			setColor(col);
+		}
+		else {
+			Color colo = getColor().darker();
+			setColor(colo);
+		}
+	}
 }
--- src/GWpart4/BlusterCritterRunner.java
+++ src/GWpart4/BlusterCritterRunner.java
@@ -1,10 +1,51 @@
 package GWpart4;
 
+import java.awt.Color;
+
+import info.gridworld.actor.Actor;
+import info.gridworld.actor.ActorWorld;
+import info.gridworld.actor.Critter;
+import info.gridworld.actor.Rock;
+import info.gridworld.grid.BoundedGrid;
+import info.gridworld.grid.Grid;
+import info.gridworld.grid.Location;
+
 public class BlusterCritterRunner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Grid<Actor> grid = new BoundedGrid<Actor>(20, 20);
+		ActorWorld world = new ActorWorld (grid);
+		
+		BlusterCritter crit = new BlusterCritter();
+		crit.setColor(Color.GREEN);
+		world.add(new Location(5, 5), crit);
+		
+		
+		for (int r = -2; r <= 2; r++) {
+			for (int c = -2; c <= 2; c++) {
+//				Actor cr = new Critter();
+				if (r != 0 && c != 0) {
+					world.add(new Location(5-r, 5-c), new Critter());
+				}
+			}
+		}
+		
+		BlusterCritter critB = new BlusterCritter();
+		critB.setColor(Color.PINK);
+		Color c = critB.getColor().brighter();
+//		critB.getColor().darker().
+		System.out.println(critB.getColor());
+		world.add(new Location(15, 15), critB);
+
+
+//		Rock c = new Rock();
+//		c.setColor(Color.RED);
+//		world.add(new Location(8, 8), c);
+//		ChameleonKid critC = new ChameleonKid();
+//		world.add(new Location(8, 7), critC);
+	
+		world.show();
 	}
 
 }
