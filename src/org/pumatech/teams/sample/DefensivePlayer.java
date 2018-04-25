ae0eb8993e738f06169d7f041a17dd0693bd7bf0
agarg01 <agarg01@P2P523FDA.admin.bvsd.k12.co.us>
2018-04-25 12:45:43 -0600

    4/25 CTF

--- src/org/pumatech/teams/sample/DefensivePlayer.java
+++ src/org/pumatech/teams/sample/DefensivePlayer.java
@@ -1,7 +1,14 @@
 package org.pumatech.teams.sample;
 
-import org.pumatech.ctf.AbstractPlayer;
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.pumatech.ctf.AbstractPlayer;
+import org.pumatech.ctf.Flag;
+import org.pumatech.ctf.Team;
+
+import info.gridworld.grid.Grid;
 import info.gridworld.grid.Location;
 
 public class DefensivePlayer extends AbstractPlayer {
@@ -11,9 +18,46 @@ public class DefensivePlayer extends AbstractPlayer {
 	}
 
 	public Location getMoveLocation() {
-		if (hasFlag())
-			return getTeam().getFlag().getLocation();
-		return getTeam().getOpposingTeam().getFlag().getLocation();
+		// if (hasFlag())
+		// return getTeam().getFlag().getLocation();
+		// return getTeam().getOpposingTeam().getFlag().getLocation();
+
+		Location myLoc = this.getLocation();
+		Grid myGrid = this.getGrid();
+		Team myTeam = this.getTeam();
+		Team theirTeam = myTeam.getOpposingTeam();
+		List<AbstractPlayer> theirPlayers = theirTeam.getPlayers();
+		Flag theirFlag = theirTeam.getFlag();
+		
+		int directionTowardsTheirFlag = myLoc.getDirectionToward(theirFlag.getLocation());
+		this.setDirection(myLoc.getDirectionToward(theirFlag.getLocation()));
+//				this.getLocation().getAdjacentLocation(Location.EAST);
+		Location locationTowardsTheirFlag = myLoc.getAdjacentLocation(getDirection());
+		if (this.getGrid().isValid(locationTowardsTheirFlag)) {
+			if (this.getGrid().get(locationTowardsTheirFlag) == null) {
+				return (locationTowardsTheirFlag);
+			}
+			else {
+				Location south = this.getLocation().getAdjacentLocation(Location.SOUTH);
+				if (this.getGrid().isValid(south)) {
+					if (this.getGrid().get(south) == null) {
+						return (south);
+					}
+					else {
+						Location north = this.getLocation().getAdjacentLocation(Location.NORTH);
+						if (this.getGrid().isValid(north)) {
+							if (this.getGrid().get(north) == null) {
+								return (north);
+							}
+							else {
+								return this.getLocation().getAdjacentLocation(Location.WEST);
+							}
+						}
+					}
+				}
+			}
+		}
+		return locationTowardsTheirFlag;
 	}
 
 }
--- src/org/pumatech/teams/sample/SampleTeam.java
+++ src/org/pumatech/teams/sample/SampleTeam.java
@@ -18,14 +18,25 @@ public class SampleTeam extends Team {
 	
 	public SampleTeam(String name, Color color) {
 		super(name, color);
-		addPlayer(new BeelinePlayer(new Location(5, 30)));
-		addPlayer(new BeelinePlayer(new Location(10, 30)));
-		addPlayer(new BeelinePlayer(new Location(15, 30)));
-		addPlayer(new RandomPlayer(new Location(20, 30)));
-		addPlayer(new RandomPlayer(new Location(30, 30)));
-		addPlayer(new RandomPlayer(new Location(35, 30)));
-		addPlayer(new RandomPlayer(new Location(40, 30)));
-		addPlayer(new RandomPlayer(new Location(45, 30)));
+		
+		addPlayer(new DefensivePlayer(new Location(5, 30)));
+		addPlayer(new DefensivePlayer(new Location(10, 30)));
+		addPlayer(new DefensivePlayer(new Location(15, 30)));
+		addPlayer(new DefensivePlayer(new Location(20, 30)));
+		addPlayer(new DefensivePlayer(new Location(30, 30)));
+		addPlayer(new DefensivePlayer(new Location(35, 30)));
+		addPlayer(new DefensivePlayer(new Location(40, 30)));
+		addPlayer(new DefensivePlayer(new Location(45, 30)));
+		
+		
+//		addPlayer(new BeelinePlayer(new Location(5, 30)));
+//		addPlayer(new BeelinePlayer(new Location(10, 30)));
+//		addPlayer(new BeelinePlayer(new Location(15, 30)));
+//		addPlayer(new RandomPlayer(new Location(20, 30)));
+//		addPlayer(new RandomPlayer(new Location(30, 30)));
+//		addPlayer(new RandomPlayer(new Location(35, 30)));
+//		addPlayer(new RandomPlayer(new Location(40, 30)));
+//		addPlayer(new RandomPlayer(new Location(45, 30)));
 	}
 
 }
